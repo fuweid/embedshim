4f6daf5837c7a28dae598b9d2b21b1b8129cbcce
Wei Fu <fuweid89@gmail.com>
2022-03-23 23:35:44 +0800

    pkg/pidfd: Add comments on API and refactor
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- pkg/pidfd/epoll.go
+++ pkg/pidfd/epoll.go
@@ -7,14 +7,20 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-type CallbackFn func() error
+type pidOnClose func() error
 
+var maxEvents = 128
+
+// Epoller is used to monitor PID file descriptors.
+//
+// When the process that PID file descriptor refers to terminates, these
+// interfaces indicate the file descriptor as readable. Then Epoller will close
+// the PID file descriptor and call the callback.
 type Epoller struct {
-	efd       int
-	closeOnce sync.Once
-
-	mu  sync.Mutex
-	cbs map[FD]CallbackFn
+	mu         sync.Mutex
+	efd        int
+	closeOnce  sync.Once
+	fdOnCloses map[FD]pidOnClose
 }
 
 func NewEpoller() (*Epoller, error) {
@@ -24,14 +30,19 @@ func NewEpoller() (*Epoller, error) {
 	}
 
 	return &Epoller{
-		efd: efd,
-		cbs: make(map[FD]CallbackFn),
+		efd:        efd,
+		fdOnCloses: make(map[FD]pidOnClose),
 	}, nil
 }
 
-func (e *Epoller) Add(fd FD, cb CallbackFn) error {
+// Add monitors the PID file descriptor and registers the onClose for it.
+func (e *Epoller) Add(fd FD, onClose func() error) error {
 	e.mu.Lock()
 	defer e.mu.Unlock()
+
+	if _, ok := e.fdOnCloses[fd]; ok {
+		return fmt.Errorf("the pidfd %v is exist", fd)
+	}
 
 	ev := unix.EpollEvent{
 		Events: unix.EPOLLIN,
@@ -39,45 +50,55 @@ func (e *Epoller) Add(fd FD, cb CallbackFn) error {
 	}
 
 	if err := unix.EpollCtl(e.efd, unix.EPOLL_CTL_ADD, int(fd), &ev); err != nil {
-		return fmt.Errorf("failed to epoll_ctl_add: %w", err)
+		return fmt.Errorf("failed to monitor pidfd: %w", err)
 	}
-	e.cbs[fd] = cb
+
+	e.fdOnCloses[fd] = onClose
 	return nil
 }
 
+// Run starts to monitor the event on PID file descriptor.
 func (e *Epoller) Run() error {
-	events := make([]unix.EpollEvent, 128)
+	events := make([]unix.EpollEvent, maxEvents)
 
 	for {
 		n, err := unix.EpollWait(e.efd, events, -1)
 		if err != nil {
-			// EINTR: The call was interrupted by a signal handler before either
-			// any of the requested events occurred or the timeout expired
+			// EINTR: The call was interrupted by a signal handler
+			// before either any of the requested events occurred
+			// or the timeout expired
 			if err == unix.EINTR {
 				continue
 			}
-			return err
+			return fmt.Errorf("failed to wait pidfd events: %w", err)
 		}
 
 		for i := 0; i < n; i++ {
-			ev := events[i]
+			fd := FD(events[i].Fd)
+
+			err := unix.EpollCtl(e.efd, unix.EPOLL_CTL_DEL, int(fd), &unix.EpollEvent{})
+			if err != nil {
+				return fmt.Errorf("failed to remove pidfd from interest list: %w", err)
+			}
 
 			e.mu.Lock()
-			cbFn := e.cbs[FD(ev.Fd)]
-			delete(e.cbs, FD(ev.Fd))
+
+			onClose := e.fdOnCloses[fd]
+			delete(e.fdOnCloses, fd)
+
 			e.mu.Unlock()
 
-			unix.EpollCtl(e.efd, unix.EPOLL_CTL_DEL, int(ev.Fd), &unix.EpollEvent{})
-			unix.Close(int(ev.Fd))
-			cbFn()
+			// TODO(fuweid): non-block mode to run onClose?
+			unix.Close(int(fd))
+			onClose()
 		}
 	}
 }
 
+// Close stops the monitor.
 func (e *Epoller) Close() error {
-	var err error
 	e.closeOnce.Do(func() {
-		err = unix.Close(e.efd)
+		unix.Close(e.efd)
 	})
-	return err
+	return nil
 }
--- pkg/pidfd/syscall.go
+++ pkg/pidfd/syscall.go
@@ -6,6 +6,11 @@ import (
 
 type FD int
 
+// Open creates a file descriptor that refers to the process whose PID is
+// specified in pid. The file descriptor is returned as the function result;
+// the close-on-exec flag is set on the file descriptor.
+//
+// https://man7.org/linux/man-pages/man2/pidfd_open.2.html
 func Open(pid uint32, flags int) (FD, error) {
 	rawFD, err := unix.PidfdOpen(int(pid), flags)
 	if err != nil {
@@ -14,6 +19,16 @@ func Open(pid uint32, flags int) (FD, error) {
 	return FD(rawFD), nil
 }
 
+// SendSignal sends a signal to a process specified by a file descriptor.
+//
+// The flags argument is reserved for future use; currently, this argument must
+// be specified as 0.
 func (fd FD) SendSignal(signal unix.Signal, flags int) error {
+	// From https://man7.org/linux/man-pages/man2/pidfd_send_signal.2.html.
+	//
+	// If the info argument is a NULL pointer, this is equivalent to
+	// specifying a pointer to a siginfo_t buffer whose fields match the
+	// values that are implicitly supplied when a signal is sent using
+	// kill(2).
 	return unix.PidfdSendSignal(int(fd), signal, nil, flags)
 }
